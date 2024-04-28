cc1cf180e36085d25b82ac47935035d9bcde3a41
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-28 13:18:23 -0700

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """Create a flask app that starts an API"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 # Create a route /status on the object app_views that returns JSON
@@ -10,3 +11,17 @@ def api_status():
     """A route that returns status OK"""
     response = {"status": "OK"}
     return jsonify(response)
+
+
+@app_views.route("/stats")
+def get_stats():
+    """A route that returns status OK"""
+    stats = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User"),
+    }
+    return jsonify(stats)
